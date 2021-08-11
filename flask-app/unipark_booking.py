1d890897f3cf7659e2a004c668ff3891d42f1b1c
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-08-11 18:04:40 +0800

    add the main script where the application instance is defined

---
+++ flask-app/unipark_booking.py
@@ -0,0 +1,32 @@
+""" Defines the Flask application instance and some tasks to help manage it.
+    This file will be executed when 'flask run' command is used
+
+    NOTE: .flaskenv used in flask commands, .env used in other application instances
+
+    Authors: Thomas Cleary, 
+"""
+
+import os
+import unittest
+
+from app import create_app
+
+
+# create an application instance with config type defined in env variable
+# (or default type - development)
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+
+@app.shell_context_processor
+def make_shell_context():
+    """ This dictionary of models will be imported into the flask shell when
+        'flask shell' cli command is used
+    """
+    return dict()
+
+
+@app.cli.command()
+def test():
+    """ define a flask comman 'flask test' to run unit tests from cmd line """
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
