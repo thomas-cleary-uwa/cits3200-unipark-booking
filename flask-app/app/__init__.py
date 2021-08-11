78b20bbb0711d474a9c9cfd854d85f6716e9e547
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-08-11 17:57:40 +0800

    create __init__ file for flask app

---
+++ flask-app/app/__init__.py
@@ -0,0 +1,32 @@
+""" Initialises an Application instance
+
+    Authors: Thomas Cleary,
+"""
+
+from flask import Flask
+
+from config import configs
+
+# initialise flask extensions here
+# eg db = SQLAlchemy()
+
+
+def create_app(config_name):
+    """ create and return an instance of the flask application """
+    # initialise the flask app
+    app = Flask(__name__)
+
+    # configure the app
+    app.config.from_object(configs[config_name])
+    configs[config_name].init_app(app)
+
+    # initalise flask extensions
+    # eg db.init(app)
+
+    # import blueprints here to avoid circular imports
+    #eg from .main import main as main_bp
+
+    # register blueprints
+    # eg app.regsiter_blueprint(main_bp)
+
+    return app
