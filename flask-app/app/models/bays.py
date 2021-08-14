b7165128a71f797eb8f0a4a47b9242a032930b5b
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-08-14 23:15:51 +0800

    add db model for parking lot

---
+++ flask-app/app/models/bays.py
@@ -0,0 +1,26 @@
+"""
+Models that relate to car bays that are managed by the application
+
+Authors: Thomas Cleary,
+
+"""
+
+from .. import db
+
+
+class ParkingLot(db.Model):
+    """ Represents a parking lot that can contain multiple car bays """
+    __tablename__ = "ParkingLot"
+
+    # Primary Key
+    id = db.Column(db.Integer, primary_key=True)
+
+    # Attributes
+    lot_number = db.Column(db.Integer, unique=True, nullable=False)
+    # location?
+
+    # Relationships
+
+
+    def __repr__(self):
+        return "<Parking Lot {}".format(self.lot_number)
