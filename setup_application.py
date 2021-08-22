94f527d086e8e7a0745aa326103a3d3ec4d4a4ba
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-08-22 12:19:44 +0800

    update setup script

--- setup_application.py
+++ setup_application.py
@@ -1,20 +1,24 @@
-from app import db
+from app import db, create_app
 from app.models.user import User, Role
 
 
 def add_admin_and_roles():
-    Role.insert_roles()
 
-    admin = User(
-        email="test@uwa.edu.au",
-        password = "admin",
-        first_name = "Uni",
-        last_name = "Park",
-        role_id = Role.query.filter_by(name="admin").first().id
-    )
+    app = create_app('heroku')
 
-    db.session.add(admin)
-    db.session.commit()
+    with app.app_context():
+        Role.insert_roles()
+
+        admin = User(
+            email="test@uwa.edu.au",
+            password = "admin",
+            first_name = "Uni",
+            last_name = "Park",
+            role_id = Role.query.filter_by(name="admin").first().id
+        )
+
+        db.session.add(admin)
+        db.session.commit()
 
 
 def  main():
