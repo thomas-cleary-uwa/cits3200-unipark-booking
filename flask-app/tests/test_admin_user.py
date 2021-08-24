1eec9553a5388e2061ccd6a532312d5bc18dbe2d
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-08-24 22:49:08 +0800

    add basic unittests for testing admin user

---
+++ flask-app/tests/test_admin_user.py
@@ -0,0 +1,85 @@
+""" Tests associated with the admin user of the application
+
+Authors: Thomas Cleary,
+"""
+
+import unittest
+
+from flask_login import current_user
+
+from app import create_app, db
+from app.models.user import User, Role
+
+
+class AdminTestCases(unittest.TestCase):
+    """ tests to run to ensure the admin account is functional """
+
+    def setUp(self):
+        # create an application and push its context on the stack
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+        # setup the database tables (testing db in memory not a file)
+        db.create_all()
+
+        Role.insert_roles()
+
+        admin_user = User(
+            email=self.app.config["ADMIN_EMAIL"],
+            password=self.app.config["ADMIN_PASSWORD"],
+            first_name="Test",
+            last_name="Admin",
+            role_id = Role.query.filter_by(name='admin').first().id
+        )
+        db.session.add(admin_user)
+        db.session.commit()
+
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+
+    def test_admin_exists(self):
+        """ test the admin user exists in the db
+
+        Test that there is at least one user who is an admin in the database.
+        Test that this admin user has: 
+            - first_name "Test", last_name="Admin", role_id=<role id of admin user>
+        """
+
+        admin_role = Role.query.filter_by(name='admin').first()
+
+        # Test that the admin exists
+        self.assertTrue(admin_role is not None)
+
+        # Test that there name is correct
+        self.assertTrue(User.query.filter_by(
+            email=self.app.config["ADMIN_EMAIL"],
+            first_name='Test', last_name='Admin',
+            role_id=admin_role.id) is not None)
+
+
+    def test_admin_can_login_out(self):
+        """ test that the admin can log in and out """
+        email    = self.app.config["ADMIN_EMAIL"]
+        password = self.app.config["ADMIN_PASSWORD"]
+        print("\n{}\n".format(password))
+
+        admin_user = User.query.filter_by(email=email).first()
+        self.assertTrue(admin_user is not None)
+
+        with self.app.test_client() as test_client:
+            # login
+            test_client.post(
+                'auth/login',
+                data=dict(email=email, password=password), follow_redirects=True
+            )
+            self.assertTrue(current_user.is_authenticated)
+            self.assertTrue(current_user.id == admin_user.id)
+
+            # logout
+            test_client.get('auth/logout', follow_redirects=True)
+            self.assertTrue(current_user.is_anonymous)
