d15cff271be62d1a1573a1cb88633d0b9a30be80
tclearyUWA <51070356+thomCleary@users.noreply.github.com>
2021-09-01 01:46:39 +0800

    add the bookings blueprint

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -36,10 +36,12 @@ def create_app(config_name):
     from .main import main as main_bp
     from .auth import auth as auth_bp
     from .admin import admin as admin_bp
+    from .bookings import bookings as bookings_bp
 
     # register blueprints
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(admin_bp, url_prefix="/admin")
+    app.register_blueprint(bookings_bp, url_prefix="/bookings")
 
     return app
