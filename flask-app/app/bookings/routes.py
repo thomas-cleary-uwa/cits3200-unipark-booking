b5c4ea3332f0175c5dead2aa0f5998ec3e17c82a
tclearyUWA <51070356+thomCleary@users.noreply.github.com>
2021-09-01 03:05:54 +0800

    add bays route to bookings blueprint and associated template

--- flask-app/app/bookings/routes.py
+++ flask-app/app/bookings/routes.py
@@ -19,3 +19,16 @@ def parking_lots():
     """ route to select a parking lot to view bay availabilties for """
     parking_lots = ParkingLot.query.all()
     return render_template("bookings/parking_lots.html", parking_lots=parking_lots)
+
+
+@bookings.route("/bays/<int:lot_number>/")
+@login_required
+def bays(lot_number):
+    """ route to view availabilities of bays in parking lot with lot number """
+    lot = ParkingLot.query.filter_by(lot_number=lot_number).first()
+
+    if lot is None:
+        flash("Something went wrong: bays for this parking lot could not be found.")
+        return redirect(url_for("bookings.parking_lots"))
+
+    return render_template("bookings/bays.html", lot=lot, bays=lot.bays)
