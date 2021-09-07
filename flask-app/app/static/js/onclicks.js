0bc0471a2ee67e340fd06d86088458412b0b13a5
tclearyUWA <51070356+thomCleary@users.noreply.github.com>
2021-09-07 22:27:13 +0800

    add unique ids to timetable cells and rows and function to log out the id when a cell is clicked

--- flask-app/app/static/js/onclicks.js
+++ flask-app/app/static/js/onclicks.js
@@ -1,4 +1,11 @@
+/* onclick functions for the app */
+
 function confirmDelete(toDelete) {
   let msg = "Are you sure you want to delete " + toDelete + "?";
   return confirm(msg);
+}
+
+
+function selectTimeslot(cellID) {
+  console.log(cellID)
 }
