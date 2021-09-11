5fc4222d0326c3e3536b7275c2a86d75cb5e912c
tclearyUWA <51070356+thomCleary@users.noreply.github.com>
2021-09-11 19:58:15 +0800

    ad js to allow user to make selections on timetables

---
+++ flask-app/app/static/js/booking.js
@@ -0,0 +1,136 @@
+/* Functions to update selected booking on booking pages
+
+Authors: Thomas Cleary,
+*/
+
+function turnCellOn(cell) {
+  cell.classList.remove("timeslot-green");
+  cell.classList.add("timeslot-yellow");
+}
+
+
+function turnCellOff(cell) {
+  cell.classList.remove("timeslot-yellow");
+  cell.classList.add("timeslot-green");
+}
+
+
+function isCellOn(cell) {
+  return (cell.classList.contains("timeslot-yellow"));
+}
+
+
+function selectTimeslot(cellID) {
+  let clickedCell = document.getElementById(cellID);
+
+  // turn the cell on
+  if (clickedCell.classList.contains("timeslot-green")) {
+    // TURN OFF OTHER CELLS NOT IN THIS ROW
+    updateTimetables(clickedCell);
+  }
+  // turn the cell off
+  else {
+    updateClickedRow(clickedCell);
+  }
+}
+
+
+function updateTimetables(clickedCell) {
+  let timetables = document.getElementsByClassName("timetable");
+  let clickedRow = clickedCell.parentElement;
+
+  for (let table of timetables) {
+    let rows = table.getElementsByTagName("tr");
+
+    for (let row of rows) {
+      let cells = row.getElementsByTagName("td");
+
+      // turn off cells not in the clicked row
+      if (!(row == clickedRow)) {
+        for (let cell of cells) {
+          if (cell.classList.contains("timeslot-yellow")) {
+            turnCellOff(cell);
+          }
+        }
+      }
+
+      // turn on cells in between previous selection and this clicked cell
+      else {
+        let turnOn = [];
+
+        let foundClicked = false;
+        let foundStart   = false;
+        let logCells     = false;
+
+        for (let cell of cells) {
+          if (cell == clickedCell) {
+            foundClicked = true;
+            logCells = true;
+            turnCellOn(cell); 
+          }
+          else if (!foundStart && cell.classList.contains("timeslot-yellow")) {
+            foundStart = true;
+            logCells = true;
+          }
+
+          if (logCells) {
+            if (!isCellOn(cell)) {
+              turnOn.push(cell);
+            }
+          }
+
+          if (foundClicked && foundStart) {
+            break;
+          }
+        }
+
+        if (foundStart) {
+          for (let cell of turnOn) {
+            turnCellOn(cell);
+          }
+        }
+      }
+    }
+  }
+}
+
+// turn of cells to left or right of clicked cell depending on which side has more
+// turn off side with less
+function updateClickedRow(clickedCell) {
+  let clickedRow = clickedCell.parentElement;
+
+  let leftOn =  [];
+  let rightOn = [];
+  let addLeft = true;
+
+  let cells = clickedRow.getElementsByTagName("td");
+
+  for (let cell of cells) {
+    if (cell == clickedCell) {
+      addLeft = false;
+      continue;
+    }
+    else if (cell.classList.contains("timeslot-yellow")) {
+      if (addLeft) {
+        leftOn.push(cell);
+      }
+      else {
+        rightOn.push(cell);
+      }
+    }
+  }
+
+  turnOffCells = [];
+  if (leftOn.length < rightOn.length) {
+    turnOffCells = leftOn;
+  }
+  else {
+    turnOffCells = rightOn;
+  }
+
+  for (let cell of turnOffCells) {
+    turnCellOff(cell);
+  }
+  turnCellOff(clickedCell);
+
+}
--- flask-app/app/static/js/onclicks.js
+++ flask-app/app/static/js/onclicks.js
@@ -1,47 +1,13 @@
-/* onclick functions for the app */
+/* onclick functions for the app
+
+Authors: Thomas Cleary,
+*/
 
 function confirmDelete(toDelete) {
   let msg = "Are you sure you want to delete " + toDelete + "?";
   return confirm(msg);
 }
 
-
-function selectTimeslot(cellID) {
-  let clickedCell = document.getElementById(cellID);
-  if (clickedCell.classList.contains("timeslot-green")) {
-    checkTimetableCells(cellID);
-    clickedCell.classList.remove("timeslot-green");
-    clickedCell.classList.add("timeslot-yellow");
-  }
-  else {
-    clickedCell.classList.remove("timeslot-yellow");
-    clickedCell.classList.add("timeslot-green");
-  }
-}
-
-
-function checkTimetableCells(clickedCellID) {
-  let timetables = document.getElementsByClassName("timetable");
-  let clickedCell = document.getElementById(clickedCellID);
-  let activeRow = clickedCell.parentElement;
-  
-  for (let i = 0; i < timetables.length; i++) {
-    let cells = timetables[i].getElementsByTagName("td");
-
-    for (let j = 0; j < cells.length; j++) {
-      let cell = cells[j];
-
-      // if cell not in the row with the clicked cell
-      // make it green
-      if (cell.parentElement.id != activeRow.id) {
-        if (cell.classList.contains("timeslot-yellow")) {
-          cell.classList.remove("timeslot-yellow");
-          cell.classList.add("timeslot-green");
-        }
-      }
-    }
-  }
-  let clickedCellIdSplit = clickedCellID.split("-");
-  let clickedCellSlot = clickedCellIdSplit[clickedCellIdSplit.length-1];
-  console.log(clickedCellSlot);
-}
+// let clickedCellIdSplit = clickedCellID.split("-");
+// let clickedCellSlot = clickedCellIdSplit[clickedCellIdSplit.length-1];
+// console.log(clickedCellSlot);
